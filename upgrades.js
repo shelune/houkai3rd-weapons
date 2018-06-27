b1078e3e4db9f9423dfdebc50a757e3eec9fb868
shelune <hieu@mios.fi>
2018-06-27 16:25:23 +0300

    tweak weapon row selectors in get wep upgrades script

--- upgrades.js
+++ upgrades.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const _ = require('lodash');
 const axios = require('axios');
+const cheerio = require('cheerio');
 
 // weapon list
 const urlSource = 'https://houkai3rd.gamewith.jp/article/show/48936';
@@ -11,9 +12,9 @@ let getWeaponUrls = async (url) => {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
     let weapons = [];
-    let weaponRows = Array.from($('#result table tbody .w1'));
+    let weaponRows = Array.from($('#result table tbody tr')).slice(1);
 
-    console.log('weapon count: ', weaponRanks.length);
+    console.log('weapon count: ', weaponRows.length);
 
     return weaponRows.map(row => {
       let url = $(row).find('td:first-child a').attr('href');
@@ -21,7 +22,7 @@ let getWeaponUrls = async (url) => {
       let atk = $(row).find('td:nth-child(3)').text();
 
       return {url, rank, atk};
-    })
+    });
 
     console.log('weapons urls: ', weapons);
     return weapons;
@@ -29,3 +30,11 @@ let getWeaponUrls = async (url) => {
     console.log('error when getting page content: ', err);
   }
 }
+
+getWeaponUrls(urlSource).then(weaponList => {
+  fs.writeFile('weapon-upgrades.json', JSON.stringify(weaponList, null, 4), (err) => {
+    if (err) {
+      console.log('error when writing file out: ', err)
+    }
+  });
+})
