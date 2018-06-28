868d9c3b7ff29889df99783a6dd9c068d79f8cce
shelune <hieu@mios.fi>
2018-06-28 15:13:19 +0300

    add upgrades to weapon

--- upgrades.js
+++ upgrades.js
@@ -31,10 +31,54 @@ let getWeaponUrls = async (url) => {
   }
 }
 
-getWeaponUrls(urlSource).then(weaponList => {
-  fs.writeFile('weapon-upgrades.json', JSON.stringify(weaponList, null, 4), (err) => {
+let getUpgradesList = async (weaponList) => {
+  const weaponListWithUpgrades = await Promise.all(weaponList.map(async weapon => {
+    return {
+      upgrades: await getUpgradeItems(weapon.url),
+      ...weapon
+    };
+  }));
+  
+  console.log('fetching done!', weaponListWithUpgrades);
+  return weaponListWithUpgrades;
+}
+
+let getUpgradeItems = async (url) => {
+  let upgrades = {};
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    let upgradeRows = Array.from($('.hk_sozai table tbody tr')).slice(1);
+
+    const upgradeCount = $(upgradeRows[0]).find('td').length - 1;
+    
+    upgradeRows.forEach((row, index) => {
+      const upgradeName = $(row).find('td:first-child a').text();
+      const upgradeImg = $(row).find('td:first-child img').attr('src');
+      upgrades[upgradeName] = {
+        requirement: Array.from($(row).find('td:not(:first-child)')).map((cell, upgradeTime) => {
+          return {[`upgrade_${upgradeTime + 1}`]: $(cell).text() === '-' ? '0' : $(cell).text()};
+        }),
+        img: upgradeImg
+      };
+    });
+    console.log('weapon upgrades: ', upgrades);
+
+    return upgrades;
+  } catch (err) {
+    console.log('getting upgrades but failed: ', err);
+  }
+}
+
+getWeaponUrls(urlSource).then(async weaponList => {
+  const weaponsWithUpgrades = await getUpgradesList(weaponList);
+  console.log('writing: ', weaponsWithUpgrades);
+
+  fs.writeFile('weapon-upgrades.json', JSON.stringify(weaponsWithUpgrades, null, 4), (err) => {
     if (err) {
       console.log('error when writing file out: ', err)
     }
   });
-})
+
+  console.log('WRITE DONE!');
+});
